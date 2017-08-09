b80cb97897605eace3ad91a49ddf4b196d290358
Sandy <SVern99@aol.com>
2017-08-09 12:50:12 +0000

    Added Devise and basic signin test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Per Devise installation instructions
+  config.action_mailer.default_url_options = { host: 'https://ruby-thursday-bara-sandyvern.c9users.io/' }
 end
