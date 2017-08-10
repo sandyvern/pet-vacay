12cba065b630e0aeffc16abb65d0822ff0de562f
Sandy <SVern99@aol.com>
2017-08-10 19:55:59 +0000

    Added Users and edited Devise registration and edit files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # get 'welcome/index'
+
   devise_for :hackers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: "home#index"
+  root to: "welcome#index"
 end
